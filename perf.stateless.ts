124ccbbe79ae35c6cdcb52a9357a42bc7133251e
Person <localboxcrox@gmail.com>
2023-11-11 15:37:55 -0500

    stuff

--- perf.stateless.ts
+++ perf.stateless.ts
@@ -6,11 +6,49 @@ import { skjdgfksg } from "./conf";
     
     console.time("libsqlBatch");
     const res = await libsqlBatch(conf, [
-        {stmt: {sql: "select * from contacts where contact_id = 3;"}},
-        {
-            stmt: {sql: "select first_name, last_name, email from contacts where contact_id = 2;"},
-        },
-        {stmt: {sql: "select * from contacts where wife_id = ?;", args: [{type: "null"}]}}
+        {stmt: {
+            sql: "select * from contacts where contact_id = ?;",
+            args: [
+                {
+                    type: "integer",
+                    value: "3"
+                }
+            ]
+        }},
+        {stmt: {
+            sql: "select first_name, last_name, email from contacts where contact_id = @koji;",
+            named_args: [
+                {
+                    name: "koji",
+                    value: {
+                        type: "integer",
+                        value: "2"
+                    }
+                }
+            ]
+        }},
+        {stmt: {
+            sql: "insert into contacts (contact_id,first_name,last_name,email,phone) values (?,?,?,?,?);",
+            args: [
+                {type: "integer", value: "6"},
+                {type: "text", value: "glomm"},
+                {type: "text", value: "feru"},
+                {type: "text", value: "moca@doro.co"},
+                {type: "text", value: "001"}
+            ]
+        }},
+        {stmt: {
+            sql: "delete from contacts where contact_id = :kkl",
+            named_args: [
+                {
+                    name: "kkl",
+                    value: {
+                        type: "integer",
+                        value: "6"
+                    }
+                }
+            ]
+        }}
     ]);
     console.timeEnd("libsqlBatch");
     
@@ -19,7 +57,15 @@ import { skjdgfksg } from "./conf";
 
 
     console.time("libsqlExecute");
-    const res2 = await libsqlExecute(conf, {sql: "select first_name, last_name, email from contacts where contact_id = 1;"});
+    const res2 = await libsqlExecute(conf, {
+        sql: "select first_name, last_name, email, contact_id from contacts where contact_id = ?;",
+        args: [
+            {
+                type: "integer",
+                value: "1"
+            }
+        ]
+    });
     console.timeEnd("libsqlExecute");
 
     console.log(res2.isOk);
