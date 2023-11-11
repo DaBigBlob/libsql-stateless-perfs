74c9342ef50ab4580babe33cd2fbdcb1f52aad8b
Person <localboxcrox@gmail.com>
2023-11-11 15:26:22 -0500

    stuff

---
+++ perf.ts
@@ -0,0 +1,43 @@
+import { createClient } from "@libsql/client/web";
+import { skjdgfksg } from "./conf";
+
+(async () => {
+    const client = createClient({
+        url: skjdgfksg.db_url,
+        authToken: skjdgfksg.authToken
+    });
+    
+    console.time("client.batch");
+    const res = await client.batch([
+        {
+            sql: "select * from contacts where contact_id = ?;",
+            args: [3]
+        },
+        {
+            sql: "select first_name, last_name, email from contacts where contact_id = @koji;",
+            args: {koji: 2}
+        },
+        {
+            sql: "insert into contacts (contact_id,first_name,last_name,email,phone) values (?,?,?,?,?);",
+            args: [6,"glomm","feru","moca@doro.co","001"]
+        },
+        {
+            sql: "delete from contacts where contact_id = :kkl",
+            args: {kkl: 6}
+        }
+    ]);
+    console.timeEnd("client.batch");
+
+    console.log(!!res);
+
+
+
+    console.time("client.execute");
+    const res2 = await client.execute({
+        sql: "select first_name, last_name, email, contact_id from contacts where contact_id = ?;",
+        args: [1]
+    });
+    console.timeEnd("client.execute");
+
+    console.log(!!res2);
+})();
