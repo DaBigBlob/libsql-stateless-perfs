e0aabf1aded849c7c1ac003b3d513ea097b99b3a
Person <localboxcrox@gmail.com>
2023-11-16 16:10:05 -0500

    stuff

--- drizzle.stuff.ts
+++ drizzle.stuff.ts
@@ -1,7 +1,6 @@
 import { createClient } from "libsql-stateless-easy";
 import { drizzle } from 'drizzle-orm/libsql';
 import { conf } from "./conf";
-import { sql } from "drizzle-orm";
 import { contacts } from "./drizzle/schema";
 
 (async () => {
