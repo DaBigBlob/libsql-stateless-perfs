6000acba85650b1d4998e98aacb83af7374ac7b6
Person <localboxcrox@gmail.com>
2024-04-02 05:30:42 -0400

    new stuff

--- error.test.ts
+++
@@ -1,8 +0,0 @@
-
-import {fetch, Request} from "cross-fetch";
-
-(async () => {
-    const req = new Request("https://api.hman.io/ip", {method: 'GET'})
-    const res = await fetch(req, {method: req.method});
-    console.log(await res.text());
-})();
